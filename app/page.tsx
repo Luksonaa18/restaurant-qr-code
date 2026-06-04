96c6c1f7dcfd727ec333a77cc9680e777b566339
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2026-06-04 18:22:28 +0400

    changes to page

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ import { Analytics } from "@vercel/analytics/next";
 export default function Home() {
   return (
     <>
-     <div classname="flex h-screen items-center justify-center w-full">
-      <h1 classname="text-red-500 text-2xl font-bold">ERROR ask for help lukazhozhadze53@gmail.com</h1>
-</div>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold text-red-500">
+          ERROR contact for help lukazhozhadze53@gmail.com
+        </h1>
+      </div>
     </>
   );
 }
