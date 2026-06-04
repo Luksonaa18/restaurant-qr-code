68ff7665e8b603a31007aa70b86fb1da793f32f9
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2026-06-04 18:26:34 +0400

    Update page 1

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold text-red-500">
+        <h1 className="text-2xl text-center font-bold text-red-500">
           ERROR contact for help lukazhozhadze53@gmail.com
         </h1>
       </div>
